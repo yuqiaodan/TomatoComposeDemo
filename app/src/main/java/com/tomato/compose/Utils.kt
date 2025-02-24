47a7b38ab579988a369a3971f6a575b5de5a2a47
yuqiaodan <yuqiaodan@testbird.com>
2025-02-24 14:39:59 +0800

    无水波纹点击监听

--- app/src/main/java/com/tomato/compose/Utils.kt
+++ app/src/main/java/com/tomato/compose/Utils.kt
@@ -2,6 +2,11 @@ package com.tomato.compose
 
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 
 /**
  * Created by Tomato on 2024/5/22.
@@ -13,4 +18,18 @@ fun log(msg:String){
 
 fun toast(msg:String){
     Toast.makeText(App.instance,msg, Toast.LENGTH_SHORT).show()
+}
+
+
+/**
+ * 无点击反馈 无点击水波纹效果的点击监听
+ * */
+fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier {
+    return composed {
+        this.clickable(
+            interactionSource = remember { MutableInteractionSource() }, // 创建一个新的无反馈的交互源
+            indication = null, // 禁用点击指示器
+            onClick = onClick
+        )
+    }
 }
